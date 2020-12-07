41c91ae8b14ef30f48fbc36359bafa407a150f68
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-12-07 19:56:49 -0300

    Checking connection error.

--- dbconnect.php
+++ dbconnect.php
@@ -7,4 +7,7 @@ $dbname = "logindb";
 
 $connect = mysqli_connect($servername, $username, $password, $dbname);
 
+if(mysqli_connect_error()):
+    echo "Connection error: ".mysqli_connect_error();
+endif;
 ?>
