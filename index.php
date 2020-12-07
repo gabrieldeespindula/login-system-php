e2ec00a33fc796f911cb1022b2fd93e13691cc57
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-12-07 20:47:40 -0300

    Error: empty login or password

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 // connecting
 require_once 'dbconnect.php';
 
+// send button
+if(isset($_POST['button-login'])):
+    $errors = array();
+    $login = mysqli_escape_string($connect, $_POST['login']);
+    $password = mysqli_escape_string($connect, $_POST['password']);
+    
+    if (empty($login) or empty($password)):
+        $errors[] = "<li> The login / password field needs to be filled. </li>";
+    else:
+    endif;
+
+endif;
 
 ?>
 
@@ -14,6 +26,16 @@ require_once 'dbconnect.php';
 
 <body>
     <h1> Login </h1>
+
+    <?php
+    if(!empty($errors)):
+        foreach($errors as $error):
+            echo $error;
+        endforeach;
+    endif;
+    ?>
+
+    <hr>
 
     <form action="" method="POST">
     Login: <input type="text" name="login"><br>
