5cecba30bb6616c381ad4b357c580aeb00beec39
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-12-07 21:22:23 -0300

    Using the name.

--- home.php
+++ home.php
@@ -1,9 +1,24 @@
+<?php
+// connecting
+require_once 'dbconnect.php';
+
+// sessions
+session_start();
+
+//data
+$id = $_SESSION['id_user'];
+$sql = "SELECT * FROM user WHERE id = '$id'";
+$result = mysqli_query($connect, $sql);
+$data = mysqli_fetch_array($result);
+?>
+
 <html>
 <head>
-    <title>Login</title>
+    <title>Restricted Page</title>
     <meta charset="utf-8">
 
 </head>
 <body>
+    <h1>Olá <?php echo $data['name']?></h1>
 </body>
 </html>
--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if(isset($_POST['button-login'])):
             if(mysqli_num_rows($result) == 1):
                 $data = mysqli_fetch_array($result);
                 $_SESSION['logged'] = true;
-                $_SESSION['id-user'] = $data['id'];
+                $_SESSION['id_user'] = $data['id'];
                 header('location: home.php');
             else:
                 $errors[] = "<li> Login and password don´t match. </li>";
